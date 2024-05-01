a5e73ce075b8d62f654ad5caf5173af492de0943
Shulpov Victor <vshulpov@gmail.com>
2024-05-01 23:29:01 +0700

    probe-1 - init spring boot project

---
+++ ssl-probe-project/src/main/java/com/xmap/sslprobeproject/SslProbeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xmap.sslprobeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslProbeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslProbeProjectApplication.class, args);
+    }
+
+}
