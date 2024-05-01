1e47247259a475b7d1b3ae6c53984e58dc8a93a5
Shulpov Victor <vshulpov@gmail.com>
2024-05-02 01:00:54 +0700

    probe-1 - add test controller and ssl settings

---
+++ ssl-probe-project/src/main/java/com/xmap/sslprobeproject/TestController.java
@@ -0,0 +1,12 @@
+package com.xmap.sslprobeproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test SSL probe project (xmap)";
+    }
+}
