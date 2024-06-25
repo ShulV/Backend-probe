3b1cd4562f1f6ccb0af31a8a07cd1cc15da3d3bb
Shulpov Victor <vshulpov@gmail.com>
2024-06-25 22:34:17 +0700

    mpp-3 - func style using activemq

--- planner-microservices/planner-todo/src/main/java/ru/javabegin/micro/planner/todo/mq/legacy/MessageConsumer.java
+++ planner-microservices/planner-todo/src/main/java/ru/javabegin/micro/planner/todo/mq/legacy/MessageConsumer.java
@@ -1,24 +1,24 @@
-package ru.javabegin.micro.planner.todo.mq.legacy;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.stereotype.Component;
-import ru.javabegin.micro.planner.todo.service.TestDataService;
-
-@Component
-@EnableBinding(TodoBinding.class)
-public class MessageConsumer {
-    private final TestDataService testDataService;
-
-    public MessageConsumer(TestDataService testDataService) {
-        this.testDataService = testDataService;
-    }
-
-    @StreamListener(target = TodoBinding.INPUT_CHANNEL)
-    public void initTestData(Long userId) throws Exception {
-        if (userId % 2 == 0) {
-            throw new Exception("dead letter queue test exception...");
-        }
-        testDataService.initTestData(userId);
-    }
-}
+//package ru.javabegin.micro.planner.todo.mq.legacy;
+//
+//import org.springframework.cloud.stream.annotation.EnableBinding;
+//import org.springframework.cloud.stream.annotation.StreamListener;
+//import org.springframework.stereotype.Component;
+//import ru.javabegin.micro.planner.todo.service.TestDataService;
+//
+//@Component
+//@EnableBinding(TodoBinding.class)
+//public class MessageConsumer {
+//    private final TestDataService testDataService;
+//
+//    public MessageConsumer(TestDataService testDataService) {
+//        this.testDataService = testDataService;
+//    }
+//
+//    @StreamListener(target = TodoBinding.INPUT_CHANNEL)
+//    public void initTestData(Long userId) throws Exception {
+//        if (userId % 2 == 0) {
+//            throw new Exception("dead letter queue test exception...");
+//        }
+//        testDataService.initTestData(userId);
+//    }
+//}
--- planner-microservices/planner-todo/src/main/java/ru/javabegin/micro/planner/todo/mq/legacy/TodoBinding.java
+++ planner-microservices/planner-todo/src/main/java/ru/javabegin/micro/planner/todo/mq/legacy/TodoBinding.java
@@ -1,14 +1,14 @@
-package ru.javabegin.micro.planner.todo.mq.legacy;
-
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.messaging.MessageChannel;
-
-// Название может быть любым
-// писывает каналы для работы с mq
-public interface TodoBinding {
-    String INPUT_CHANNEL = "todoInputChannel";// Нужен, чтобы ссылаться на него, а не везде
-
-    // Создаем канал для приема сообщений
-    @Input(INPUT_CHANNEL)
-    MessageChannel todoInputChannel();// Название канала может браться из названия метода
-}
+//package ru.javabegin.micro.planner.todo.mq.legacy;
+//
+//import org.springframework.cloud.stream.annotation.Input;
+//import org.springframework.messaging.MessageChannel;
+//
+//// Название может быть любым
+//// писывает каналы для работы с mq
+//public interface TodoBinding {
+//    String INPUT_CHANNEL = "todoInputChannel";// Нужен, чтобы ссылаться на него, а не везде
+//
+//    // Создаем канал для приема сообщений
+//    @Input(INPUT_CHANNEL)
+//    MessageChannel todoInputChannel();// Название канала может браться из названия метода
+//}
